34e239c00593b53d75ec78d131b94ae536f2ce88
Jose <jsegarra1971@gmail.com>
2022-02-24 12:45:03 +0100

    C2

---
+++ lib/files.pas
@@ -0,0 +1,13 @@
+unit files;
+interface
+
+implementation
+
+initialization
+	// here may be placed code that is
+	// executed as the unit gets loaded
+
+finalization
+	// code executed at program end
+
+end.
