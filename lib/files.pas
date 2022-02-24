adf0f00da7af77127319fde263dd47c3824b36d0
Jose <jsegarra1971@gmail.com>
2022-02-24 12:45:03 +0100

    C2

---
+++ lib/files.pas
@@ -0,0 +1,17 @@
+unit files;
+interface
+
+
+type
+	// the type TRandomNumber gets globally known
+	// since it is included somewhere (uses-statement)
+	TRandomNumber = integer;
+
+    
+implementation
+
+initialization
+
+finalization
+
+end.
